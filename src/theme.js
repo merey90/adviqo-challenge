b8107b3cf0786c4adde4d5a63076c92e751206cd
Merey <merey.90@gmail.com>
2019-05-03 17:43:26 +0200

    creating material ui theme

---
+++ src/theme.js
@@ -0,0 +1,31 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#282c34'
+    },
+    secondary: {
+      main: '#5e0949'
+    },
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    useNextVariants: true
+  },
+  
+});
+
+export default theme;
